c354f57ec6f891e2bee56f334e1152090fef4e58
eben-vranken <vranken.eben@outlook.com>
2026-07-11 12:50:26 +0200

    feat: add optional input field and handle optional in-line sequence OR input field logic

--- codon-translator.py
+++ codon-translator.py
@@ -5,14 +5,23 @@ from src import translator
 def parse_args():
     parser = ArgumentParser()
 
-    parser.add_argument("sequence", help="The nucleotide sequenced to be translated")
+    parser.add_argument("sequence", nargs="?", help="The nucleotide sequenced to be translated")
     
     parser.add_argument("-s", "--short", action="store_true", help="Shorten the amino acids to their three letter abbreviations")
     parser.add_argument("-f", "--fasta", action="store_true", help="Shorten the amino acids to their standard FASTA format")
-    return parser.parse_args()
+    parser.add_argument("-i", "--input", help="The file from which to read the amino acids")
+    
+    args = parser.parse_args()
+
+    if not args.sequence and not args.input:
+        parser.error("You must provide either a 'sequence' positional argument or an '--input' file.")
+
+    return args
 
 if __name__ == "__main__":
     args = parse_args()
+
+    print(args.input)
 
     codon_list = translator.parse(args.sequence)
     amino_list = translator.translate(codon_list)
